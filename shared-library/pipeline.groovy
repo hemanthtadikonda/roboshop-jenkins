87cbffef7764db49a718fcc397dfdaaed27186ab
hemanth <hemanthtadikonda29@gmail.com>
2023-09-23 13:02:50 +0530

    shared-library

--- shared-library/pipeline.groovy
+++ shared-library/pipeline.groovy
@@ -1,13 +1,16 @@
 def call () {
     pipeline {
         agent any
-        satges{
+
+        stages {
             stage('code compile') {
                 steps {
                     sh 'env'
                     // depends upon component type
                     print 'OK'
                 }
+            }
+
             stage('Test cases') {
                 when  {
                     expression { env.TAG_NAME != null}
@@ -17,13 +20,14 @@ def call () {
                     print 'OK'
                 }
             }
+
             stage ('code quality') {
                 when {
                     allOf {
-                        expression { env.BRANCH_NAME == "main"}
-                        expression { env.TAG_NAME    != null}
+                        expression { env.BRANCH_NAME != null }
+                        expression { env.TAG_NAME == null }
+                    }
                 }
-
                 //parameters {
                 //    password(name: 'SONAR.PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
                 //}
@@ -31,21 +35,24 @@ def call () {
                     //sh 'sonar scan command depends code type'
                     print 'OK'
                 }
-            stage ('code security') {
-                    when {
-                        allOf {
-                            expression { env.BRANCH_NAME == "main"}
-                            expression { env.TAG_NAME    != null}
-                        }
-                    }
-                    //parameters {
-                    //    password(name: 'SONAR.PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
-                    //}
-                    steps {
-                        //sh 'checkmarx sast sca'
-                        print 'OK'
-                    }
             }
+
+            stage ('code security') {
+                when {
+                    allOf {
+                        expression { env.BRANCH_NAME == "main"}
+                        expression { env.TAG_NAME    == null}
+                    }
+                }
+                //parameters {
+                //    password(name: 'SONAR.PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+                //}
+                steps {
+                    //sh 'checkmarx sast sca'
+                    print 'OK'
+                }
+            }
+
             stage ('Realse') {
                 when {
                     expression {env.TAG_NAME ==~ ".*"}
