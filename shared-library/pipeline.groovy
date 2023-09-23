9442d78079e1f7a9512f9a06a43b88d4e30b7939
hemanth <hemanthtadikonda29@gmail.com>
2023-09-23 12:52:38 +0530

    shared-library

---
+++ shared-library/pipeline.groovy
@@ -0,0 +1,62 @@
+def call () {
+    pipeline {
+        agent any
+        satges{
+            stage('code compile') {
+                steps {
+                    sh 'env'
+                    // depends upon component type
+                    print 'OK'
+                }
+            stage('Test cases') {
+                when  {
+                    expression { env.TAG_NAME != null}
+                }
+                steps {
+                    // depends upon component type
+                    print 'OK'
+                }
+            }
+            stage ('code quality') {
+                when {
+                    allOf {
+                        expression { env.BRANCH_NAME == "main"}
+                        expression { env.TAG_NAME    != null}
+                }
+
+                //parameters {
+                //    password(name: 'SONAR.PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+                //}
+                steps {
+                    //sh 'sonar scan command depends code type'
+                    print 'OK'
+                }
+            stage ('code security') {
+                    when {
+                        allOf {
+                            expression { env.BRANCH_NAME == "main"}
+                            expression { env.TAG_NAME    != null}
+                        }
+                    }
+                    //parameters {
+                    //    password(name: 'SONAR.PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+                    //}
+                    steps {
+                        //sh 'checkmarx sast sca'
+                        print 'OK'
+                    }
+            }
+            stage ('Realse') {
+                when {
+                    expression {env.TAG_NAME ==~ ".*"}
+                }
+                steps {
+                    // sh 'upload artifact to nexus command
+                    print 'OK'
+                }
+
+            }
+        }
+
+    }
+}
