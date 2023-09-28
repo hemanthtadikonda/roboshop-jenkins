a8cce2932fc1c7e38d3e9a08dbca368f2034dbff
hemanth <hemanthtadikonda29@gmail.com>
2023-09-28 09:34:29 +0530

    scripted pipeline

--- shared-library/vars/demo.groovy
+++ shared-library/vars/demo.groovy
@@ -1,4 +1,14 @@
 node {
+   sh "find . | sed -e '1d' |xargs rm -rf"
+   if(env.TAG_NAME ==~ ".*") {
+      env.branch_name = "refs/tags/${env.TAG_NAME}"
+   } else {
+      env.branch_name = "${env.BRANCH_NAME}"
+   }
+   checkout scmGit(
+           branches: [[name: branch_name]],
+           userRemoteConfigs: [[url: "https://github.com/hemanthtadikonda/shipping"]]
+   )
    properties([
       parameters([
          [$class      : 'PasswordParameterDefinition',
