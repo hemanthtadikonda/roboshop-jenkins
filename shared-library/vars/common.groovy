fceec1391b291185e0cca8dc4f2e580c04ba7a0c
hemanth <hemanthtadikonda29@gmail.com>
2023-09-29 08:10:28 +0530

    scripted pipeline

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -4,7 +4,7 @@ def compile() {
    }
    stage('compile'){
       if(env.codeType == "maven") {
-         sh '/root/maven/bin/mvn package'
+         sh '/home/centos/maven/bin/mvn package'
       }
       if(env.codeType == "nodejs"){
          sh 'npm install'
