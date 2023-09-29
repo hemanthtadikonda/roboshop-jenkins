079eef4c29fdc1b1de867528bed8e37dbc9dcd6c
hemanth <hemanthtadikonda29@gmail.com>
2023-09-29 07:58:46 +0530

    scripted pipeline

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,15 +1,18 @@
 def call(){
-   if(env.BRANCH_NAME == "main"){
-      common.compile()
-      common.test()
-      common.codequality()
-      common.codesecurity()
-   }else {
-      common.compile()
-      common.test()
-   }
-   if (env.TAG_NAME==~ ".*") {
-      common.compile()
-      common.release()
+   node('work station') {
+
+      if (env.BRANCH_NAME == "main") {
+         common.compile()
+         common.test()
+         common.codequality()
+         common.codesecurity()
+      } else {
+         common.compile()
+         common.test()
+      }
+      if (env.TAG_NAME ==~ ".*") {
+         common.compile()
+         common.release()
+      }
    }
 }
