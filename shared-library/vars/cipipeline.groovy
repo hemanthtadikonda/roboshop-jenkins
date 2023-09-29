74e0a4e4ce1e80e0958b57b79d9d9464bce71842
hemanth <hemanthtadikonda29@gmail.com>
2023-09-29 08:43:34 +0530

    scripted pipeline

--- shared-library/vars/cipipeline.groovy
+++ shared-library/vars/cipipeline.groovy
@@ -1,5 +1,15 @@
 def call(){
    node('work station') {
+      sh "find . | sed -e '1d' |xargs rm -rf"
+      if(env.TAG_NAME ==~ ".*") {
+         env.branch_name = "refs/tags/${env.TAG_NAME}"
+      } else {
+         env.branch_name = "${env.BRANCH_NAME}"
+      }
+      checkout scmGit(
+              branches: [[name: branch_name]],
+              userRemoteConfigs: [[url: "https://github.com/raghudevopsb74/${component}"]]
+      )
 
       if (env.BRANCH_NAME == "main") {
          common.compile()
