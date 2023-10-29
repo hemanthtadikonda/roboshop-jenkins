eba606ee18d6426d7d9234bd52990d5b13c99674
hemanth <hemanthtadikonda29@gmail.com>
2023-10-29 09:33:41 +0530

    Jenkins release pipeline

--- shared-library/vars/common.groovy
+++ shared-library/vars/common.groovy
@@ -50,18 +50,23 @@ def codesecurity(){
 
 def release(){
    stage('Release') {
-      env.nexususer = sh (script: 'aws ssm get-parameter --name "nexus.dev.username" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-      env.nexuspass = sh (script: 'aws ssm get-parameter --name "nexus.dev.password" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
-      wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: nexuspass]]]) {
-         sh 'echo ${TAG_NAME} >VERSION'
-         if (env.codeType == "maven") {
-            sh 'cp target/${component}-1.0.jar ${component}.jar; zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schemadir}'
-         } else if (env.codeTppe == "nodejs") {
-            sh 'zip -r ${component}-${TAG_NAME}.zip server.js node_modules VERSION ${schemadir}'
-         } else {
-            sh 'zip -r ${component}-${TAG_NAME}.zip *'
-         }
-         sh 'curl -v -u ${nexususer}:${nexuspass} --upload-file ${component}-${TAG_NAME}.zip http://172.31.87.150:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-      }
+//      env.nexususer = sh (script: 'aws ssm get-parameter --name "nexus.dev.username" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+//      env.nexuspass = sh (script: 'aws ssm get-parameter --name "nexus.dev.password" --with-decryption --query="Parameter.Value" |xargs', returnStdout: true).trim()
+//      wrap([$class: "MaskPasswordsBuildWrapper", varPasswordPairs: [[password: nexuspass]]]) {
+//         sh 'echo ${TAG_NAME} >VERSION'
+//         if (env.codeType == "maven") {
+//            sh 'cp target/${component}-1.0.jar ${component}.jar; zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${schemadir}'
+//         } else if (env.codeTppe == "nodejs") {
+//            sh 'zip -r ${component}-${TAG_NAME}.zip server.js node_modules VERSION ${schemadir}'
+//         } else {
+//            sh 'zip -r ${component}-${TAG_NAME}.zip *'
+//         }
+//         sh 'curl -v -u ${nexususer}:${nexuspass} --upload-file ${component}-${TAG_NAME}.zip http://172.31.87.150:8081/repository/${component}/${component}-${TAG_NAME}.zip'
+//      }
+      sh 'echo ${TAG_NAME} >VERSION'
+      aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 043254050286.dkr.ecr.us-east-1.amazonaws.com
+      docker build -t 043254050286.dkr.ecr.us-east-1.amazonaws.com/{componet}:VERSION .
+      docker push 043254050286.dkr.ecr.us-east-1.amazonaws.com/{componet}:VERSION
+
    }
 }
